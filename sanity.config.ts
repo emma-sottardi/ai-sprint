34029f5beacca657772f21eae87b6ce6338ebe13
Emma <emma@emmasottardi.com>
2026-04-30 11:42:27 +0200

    Updates from Ship Studio

--- sanity.config.ts
+++ sanity.config.ts
@@ -5,6 +5,7 @@ import { schemaTypes } from "./sanity/schemaTypes";
 export default defineConfig({
   name: "h-studio",
   title: "H.Studio",
+  basePath: "/studio",
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   plugins: [structureTool()],
